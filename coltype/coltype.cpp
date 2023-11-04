4551dc0d19458773e01604291e72cfa8e79268a4
Onii Chan <46487694+sogood99@users.noreply.github.com>
2023-11-03 23:23:05 -0400

    update

--- coltype/coltype.cpp
+++ coltype/coltype.cpp
@@ -24,5 +24,65 @@ int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
 
+  string s;
+  cin >> s;
+  reverse(s.begin(), s.end());
+
+  int n = s.size();
+
+  for (char c : s) {
+    if (c != 'O' && c != 'E') {
+      cout << "INVALID" << endl;
+      return 0;
+    }
+  }
+  if (s[0] != 'O') {
+    cout << "INVALID" << endl;
+    return 0;
+  }
+
+  ll mv = LLINF;
+
+  for (ll k = 1; k < LLINF; k *= 2) {
+    ll c = k;
+    bool flag = false;
+    for (int i = 0; i < n; i++) {
+      if (s[i] == 'O') {
+        if ((c - 1) % 3 == 0) {
+          c = (c - 1) / 3;
+        } else {
+          flag = true;
+          break;
+        }
+        if (c % 2 != 1) {
+          flag = true;
+          break;
+        }
+      } else {
+        if (c == LSOne(c)) {
+          flag = true;
+          break;
+        } else {
+          c *= 2;
+        }
+
+        if (c < 0 || c % 2 != 0) {
+          flag = true;
+          break;
+        }
+      }
+    }
+    // cout << k << " " << c << endl;
+    if (!flag) {
+      mv = min(c, mv);
+    }
+  }
+
+  if (mv > (1ll << 47)) {
+    cout << "INVALID" << endl;
+  } else {
+    cout << mv << endl;
+  }
+
   return 0;
 }
