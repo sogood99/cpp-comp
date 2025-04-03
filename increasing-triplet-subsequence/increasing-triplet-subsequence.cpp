1c792a1a331ce0b081606781c67d6908ed52366d
Onii Chan <46487694+sogood99@users.noreply.github.com>
2025-04-03 19:01:02 -0400

    Update

---
+++ increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,51 @@
+#include <bits/extc++.h>
+#include <bits/stdc++.h>
+
+#define LSOne(S) ((S) & -(S))
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    ost;
+
+// Shortcuts for "common" constants
+const int INF = 1e9;   // 10^9 = 1B is < 2^31-1
+const ll LLINF = 4e18; // 4*10^18 is < 2^63-1
+const double EPS = 1e-9;
+
+class Solution {
+public:
+  bool increasingTriplet(vector<int> &nums) {
+    int low = INT_MAX, sndlow = INT_MAX;
+
+    for (int i : nums) {
+      if (i == low || i == sndlow) {
+        continue;
+      }
+
+      if (i < low) {
+        low = i;
+      } else if (i < sndlow) {
+        sndlow = i;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  return 0;
+}
